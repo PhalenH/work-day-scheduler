0b0f8e462316570bec52110eee0d922e6c9d5a17
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-04 17:31:07 -0400

    corrected link to css stylesheet, added function which will determine background color for inputs based on time of day

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,9 @@
+console.log("Hello world")
 var today = moment();
 
 $('#currentDay').text(today.format("dddd, MMM Do"));
 
+// $('button').parent().on("click", function(event)
 $('button').on("click", function(event){
     saveTask();
 });
@@ -9,14 +11,28 @@ $('button').on("click", function(event){
 function saveTask() {
     localStorage.setItem("task", activity.value);
   }
-
-
-
-  // if, else if, else statement to color input boxes based on if
-  // time has passed (gray), in current time window(red), or future time (green)
-
-  // for loop to reduce html documentation?
-
   // save input in console log
   // Should I save each one seperately or in an array
+  // if seperate, need ability to know which button was clicked, give parens element ID to match time
   // if array could I assign each time with an index so will always match up
+
+
+
+
+var CurrentTime = $('h2').getAttribute("data-time")
+
+function timeColor(){
+    if (moment().format('h a') > CurrentTime) {
+        $('#actvity').css('background-color', 'gray');
+    }
+    else if (moment().format('h a') < CurrentTime) {
+        $('#actvity').css('background-color', 'green');
+    }
+    else {
+        $('#actvity').css('background-color', 'red');
+    }
+}
+// if, else if, else statement to color input boxes based on if
+// time has passed (gray), in current time window(red), or future time (green)
+
+// for loop to reduce html documentation?
