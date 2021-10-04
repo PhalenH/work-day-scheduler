d49199893a0dec088e64e3d608b8afb72abff313
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-04 16:44:33 -0400

    replaced repeated ul with repeated sections containing h2/input/button, added commented out code in js of things I need todo as well as button for when button is clicked to setItem in local storage which I need to edit

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,22 @@
 var today = moment();
+
 $('#currentDay').text(today.format("dddd, MMM Do"));
+
+$('button').on("click", function(event){
+    saveTask();
+});
+
+function saveTask() {
+    localStorage.setItem("task", activity.value);
+  }
+
+
+
+  // if, else if, else statement to color input boxes based on if
+  // time has passed (gray), in current time window(red), or future time (green)
+
+  // for loop to reduce html documentation?
+
+  // save input in console log
+  // Should I save each one seperately or in an array
+  // if array could I assign each time with an index so will always match up
