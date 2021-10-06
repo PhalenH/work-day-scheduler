26d7dc37b8991c2a1d0ffac430c292712f89ee90
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-05 20:22:41 -0400

    changed sotrage value in saveTask function, added empty array to store tasks and if statement to check if it's there, added for loop to get inputs from each time and appended those to the array, not working right now though, getting array of null when saved

--- assets/main.js
+++ assets/main.js
@@ -1,52 +1,61 @@
 console.log("Hello world");
-var today = moment();
 
+var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
-// $('button').parent().on("click", function(event)
+// $("button").on("click", function (event) {
+//   saveTask();
+// });
+
+// need empty array for local storage to add input too
+var task = [];
+if (localStorage.getItem("task") != null) {
+  task = JSON.parse(localStorage.getItem("task"));
+}
+
 $("button").on("click", function (event) {
-  saveTask();
+  for (var i = 0; i < 24; i++) {
+    var taskInput = "";
+    taskInput = $("#activity-" + i).value;
+    // taskInput = JSON.stringify($("#activity-" + i).value);
+    console.log(taskInput);
+    task.push(taskInput);
+    saveTask();
+  }
 });
+// Possibly another for loop needed to keep value on page, like using: something.text($("#activity-" + i).value)?
 
 function saveTask() {
-  localStorage.setItem("task", activity.value);
+  localStorage.setItem("task", JSON.stringify(task));
 }
-// save input in console log
-// Use an array
-// if array could I assign each time with an index so will always match up
-// need empty array for local storage to add input too
-var task = [] 
- 
 
 
 
 
-var CurrentTime = document.getElementsByTagName("h2")
+
+var CurrentTime = document.getElementsByTagName("h2");
 console.log(CurrentTime);
-console.log(moment().format("k"))
+console.log(moment().format("k"));
 
 function timeColor() {
-    for (var i = 0; i < 24; i++) {
-      // var inputText = "activity-" + i;
-  
-      // used parseInt to turn all data-time strings to integers to be compared with current time
-  var timeData = parseInt(CurrentTime[i].getAttribute("data-time"))
-        console.log(timeData)      
+  for (var i = 0; i < 24; i++) {
+    // var inputText = "activity-" + i;
 
-      if (moment().format("k") > timeData) {
-          console.log("past")
-        $("#activity-" + i).addClass("past-time");
-      } 
-      else if (moment().format("k") < timeData) {
-          console.log("future")
-        $("#activity-" + i ).addClass("present-time");
-      } else {
-          console.log("present")
-        $("#activity-" + i).addClass("future-time");
-      }
+    // used parseInt to turn all data-time strings to integers to be compared with current time
+    var timeData = parseInt(CurrentTime[i].getAttribute("data-time"));
+
+    if (moment().format("k") > timeData) {
+      console.log("past");
+      $("#activity-" + i).addClass("past-time");
+    } else if (moment().format("k") < timeData) {
+      console.log("future");
+      $("#activity-" + i).addClass("present-time");
+    } else {
+      console.log("present");
+      $("#activity-" + i).addClass("future-time");
     }
   }
-  timeColor();
-
+}
+timeColor();
 
 // for loop to reduce html documentation?
